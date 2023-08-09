bd897035fc86a0c714e7f4244f029d60ed5e934a
santiago01est <santiago01est@gmail.com>
2023-08-08 20:56:32 -0500

    add template

---
+++ src/test/java/is/demo/serenity/runners/Runner.java
@@ -0,0 +1,16 @@
+package is.demo.serenity.runners;
+
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+    features = "src/test/resources/features/",
+        tags = "@smokeTest",
+        glue = "is.demo.serenity.stepDefinitions",
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+public class Runner {
+}
